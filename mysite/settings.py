5b2ad5dc62eebd0fbb19c689f3121e19db566f27
USER <omonboy2003@gmail.com>
2023-05-03 01:56:27 +0500

    Omonboy

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-h!rp#78s6yesrz08^ol(8$oh@chpg#-jj*yozoly=zi#o-tv4k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
